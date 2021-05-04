a3bda9bbd84867d411fabe84cd50ae20ffced40f
Kevin <kevinstyersgmu@gmail.com>
2021-05-04 13:05:01 -0600

    add user usage log

--- server.R
+++ server.R
@@ -4,8 +4,32 @@ swft.server.folder.path="./"
 Sys.setenv("AWS_ACCESS_KEY_ID"     = "research-eddy-inquiry",
            "AWS_S3_ENDPOINT"       = "neonscience.org",
            "AWS_DEFAULT_REGION"    = "s3.data")
+
+if(file.exists("./data/user_log/user_log.RDS") == FALSE){
+  empty_data = data.table::data.table()
+  
+  base::saveRDS(empty_data, file = "./data/user_log/user_log.RDS")
+  
+}
+
 # Defined server logic that loads each seperate tab from it's own server file. 
-server <- function(input, output,session) {
+server <- function(input, output, session) {
+  server_start_time = Sys.time()
+  session$onSessionEnded(function() {
+    server_end_time = Sys.time()
+    users_data_in = data.table::data.table("Start" = server_start_time, "End" = server_end_time, "Total Time" = difftime(server_end_time, server_start_time, units = "secs"))
+
+    message("Saving using master")
+    # Read in master file
+    all_users_data = base::readRDS(file = "./data/user_log/user_log.RDS")
+    # Combine the new data to the master
+    users_data_out = data.table::rbindlist(l = list(all_users_data, users_data_in))
+    # Save out the new file
+    base::saveRDS(object = users_data_out, file = "./data/user_log/user_log.RDS")
+
+
+  })
+  
   base::source(file='./server/swiftCvalFst.R',        local = TRUE)
   base::source(file='./server/swiftAqua.R',           local = TRUE)
   base::source(file='./server/swftEddyCo.R',          local = TRUE)
